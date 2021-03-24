0fc2738245d615bdced986cd4433f3236fccda71
Francesco Terenzi <fra.terenz1993@gmail.com>
2021-03-24 18:38:44 +0100

    ex3 and 6 in rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,33 @@
+// from https://github.com/fraterenz/numpy-100/blob/master/100_Numpy_exercises.md
+#![allow(dead_code)]
+use ndarray::Array1;
+
+fn ex3() -> Array1<u32> {
+    // array![] cannot default with length I think
+    Array1::default(10)
+}
+
+fn ex6() -> Array1<u32> {
+    let mut a = Array1::zeros(10);
+    a[4] = 1;
+    a
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_ex3() {
+        let a = ex3();
+        assert_eq!(10, a.len());
+        assert_ne!(1, a.len());
+    }
+
+    #[test]
+    fn test_ex6() {
+        let a = ex6();
+        assert_eq!(1, a.sum());
+        assert_eq!(1, a[4]);
     }
 }
